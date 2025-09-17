7209ee02936430af09c52e444b5bdc57819cfd7e
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-09-17 16:22:57 +0100

    GDCB-11 Modifier le retement des methodes de classe client pour les exception

--- src/model/Client.java
+++ src/model/Client.java
@@ -6,7 +6,11 @@ public class Client {
     private static HashMap<String, Compte> accountes = new HashMap<String, Compte>();
 
     public void ajouterCompte(Compte compte) {
-        accountes.put(compte.getCode(), compte);
+        try {
+            accountes.put(compte.getCode(), compte);
+        } catch (Exception exception) {
+            System.out.println("Quelque chose s'est mal passé. " + exception.getMessage());
+        }
     }
 
     public HashMap<String, Compte> getComptes() {
@@ -14,12 +18,21 @@ public class Client {
     }
 
     public Compte findCompte(String code) {
-        return accountes.get(code);
+        try {
+            return accountes.get(code);
+        } catch (Exception exception) {
+            System.out.println("Quelque chose s'est mal passé. " + exception.getMessage());
+        }
+        return null;
     }
 
     public void affichierAccountes() {
-        for (Compte c : accountes.values()) {
-            c.afficherDetails();
-        };
+        if (!accountes.isEmpty()) {
+            for (Compte c : accountes.values()) {
+                c.afficherDetails();
+            }
+        } else {
+            System.out.println("Il n'y a pas de compte bancaire dans le systeme");
+        }
     }
 }
