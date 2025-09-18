6c54bab3371f5efcfdadfddf9ffa334f2c171664
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-09-19 00:48:53 +0100

    GDCB-13 Realiser l'ajout authomatique de l'intert pour les comptes epargne

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
 import controller.BanqueController;
 import model.Client;
+import model.Compte;
+import model.CompteEpargne;
+
+import java.util.HashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         Client client = new Client();
-        BanqueController banqueController = new BanqueController(client);
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(() -> {
+            HashMap<String, Compte> accounts = client.getComptes();
+            for (Compte compte : accounts.values()) {
+                if (compte instanceof CompteEpargne) {
+                    double interet = compte.calculerInteret();
+                    compte.setSolde(compte.getSolde() + interet);
+                }
+            }
+        }, 0, 5, TimeUnit.SECONDS);
 
+        BanqueController banqueController = new BanqueController(client);
         banqueController.menuGeneral();
 
 
