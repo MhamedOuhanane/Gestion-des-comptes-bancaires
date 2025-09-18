4761d1689604d5fd4122ea607930f8ae20c71d38
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-09-18 12:01:38 +0100

    GDCB-12 Ajouter la logique metier (service) pour le compte

---
+++ src/service/CompteService.java
@@ -0,0 +1,37 @@
+package service;
+
+import model.Client;
+import model.Compte;
+
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Random;
+
+public class CompteService {
+    private Client client;
+
+    public CompteService(Client client) {
+        this.client = client;
+    }
+
+    public void createCompte(Compte compte) {
+        try {
+            this.client.ajouterCompte(compte);
+        } catch (Exception exception) {
+            System.out.println("Quelque chose s'est mal passé. " + exception.getMessage());
+        }
+    }
+
+    public String genererCodeCompte() {
+        Random random = new Random();
+        int initialRang = random.nextInt(90000) + 10000;
+        String code = "CPT-" + initialRang;
+        HashMap<String, Compte> accounts = this.client.getComptes();
+
+        while (accounts.containsKey(code)) {
+            int rang = random.nextInt(90000) + 10000;
+            code = "CPT-" + rang;
+        }
+        return code;
+    }
+}
